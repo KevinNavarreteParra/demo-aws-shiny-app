de157e314f57d1261637463927786afa6aa88ca0
Kevin Navarrete-Parra <140230083+KevinNavarreteParra@users.noreply.github.com>
2024-02-01 20:46:01 -0800

    Create ui.R

---
+++ app/ui.R
@@ -0,0 +1,6 @@
+ui <- fluidPage(
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30), 
+  sliderInput("y", label = "and y is", min = 1, max = 50, value = 30),
+  "then x times y is",
+  textOutput("product")
+)
